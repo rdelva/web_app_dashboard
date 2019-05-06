d0694f92ea81d14822e958e65dbfae375b0cfd42
rdelva <rdelva@users.noreply.github.com>
2019-05-06 00:50:37 -0400

    update mobile chart with new data

--- js/scripts.js
+++ js/scripts.js
@@ -105,13 +105,11 @@ function mobileUsers() {
 var myChart = new Chart(ctx, {
     type: 'doughnut',
     data: {
-        labels: ['16-22', '23-29', '30-5', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
+        labels: ['Phone', 'Tablets', 'Desktop'],
         datasets: [{
-            tension: 0,
-            radius : 6, 
-         	pointBackgroundColor: 'rgba(0, 0, 0, 0)',
+                   	
             backgroundColor : 'rgba(116, 119, 191, .2)',             
-            data: [800, 1300, 1000, 1500, 1900, 1500, 1800, 1300, 1800, 1300, 1700, 2400],
+            data: [15, 10, 75],
             borderColor: [
                'rgba(116, 119, 191, 1)',
                'rgba(116, 119, 191, 1)',
