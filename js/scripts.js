29f3606252638b8a9799cff0e3d750c08fd300bc
rdelva <rdelva@users.noreply.github.com>
2019-05-05 22:18:43 -0400

    change background color of  chart

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@ $(".menu-btn").click( function(){
 });
 
 
-var ctx = document.getElementById('myChart');
+var ctx = document.getElementById('trafficChart');
 var myChart = new Chart(ctx, {
     type: 'line',
     data: {
@@ -18,22 +18,23 @@ var myChart = new Chart(ctx, {
             label: '# of Votes',
             data: [800, 1300, 1000, 1500, 1900, 1500, 1800, 1300, 1800, 1300, 1700, 2400],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
+                'rgba(77, 76, 114, 0.2)',
+                'rgba(77, 76, 114, 0.2)',
+                'rgba(77, 76, 114, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
                 'rgba(153, 102, 255, 0.2)',
                 'rgba(255, 159, 64, 0.2)'
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
+                'rgba(77, 76, 114, 0.2)',
                 'rgba(54, 162, 235, 1)',
                 'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
                 'rgba(153, 102, 255, 1)',
                 'rgba(255, 159, 64, 1)'
             ],
-            borderWidth: 1
+            borderWidth: 3
         }]
     },
     options: {
