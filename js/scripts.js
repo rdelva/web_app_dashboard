eb5bfa274191b098ba8704fb8dd541687ed5bef0
rdelva <rdelva@users.noreply.github.com>
2019-05-06 00:10:57 -0400

    add in chart for daily traffic

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,9 @@ var myChart = new Chart(ctx, {
         labels: ['16-22', '23-29', '30-5', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
         datasets: [{
             tension: 0,
-            radius : 6,           
+            radius : 6, 
+         	pointBackgroundColor: 'rgba(0, 0, 0, 0)',
+            backgroundColor : 'rgba(116, 119, 191, .2)',             
             data: [800, 1300, 1000, 1500, 1900, 1500, 1800, 1300, 1800, 1300, 1700, 2400],
             borderColor: [
                'rgba(116, 119, 191, 1)',
@@ -32,7 +34,6 @@ var myChart = new Chart(ctx, {
                'rgba(116, 119, 191, 1)',
                'rgba(116, 119, 191, 1)',
             ],
-
             borderWidth: 1
         }]
     },
@@ -51,4 +52,51 @@ var myChart = new Chart(ctx, {
 });
 
 
-	
+function dailyTraffic (){
+
+
+	var ctx = document.getElementById('dailyTraffic');
+	var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['16-22', '23-29', '30-5', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
+        datasets: [{
+            tension: 0,
+            radius : 6, 
+         	pointBackgroundColor: 'rgba(0, 0, 0, 0)',
+            backgroundColor : 'rgba(116, 119, 191, .2)',             
+            data: [800, 1300, 1000, 1500, 1900, 1500, 1800, 1300, 1800, 1300, 1700, 2400],
+            borderColor: [
+               'rgba(116, 119, 191, 1)',
+               'rgba(116, 119, 191, 1)',
+               'rgba(116, 119, 191, 1)',
+               'rgba(116, 119, 191, 1)',
+               'rgba(116, 119, 191, 1)',
+               'rgba(116, 119, 191, 1)',
+               'rgba(116, 119, 191, 1)',
+               'rgba(116, 119, 191, 1)',
+               'rgba(116, 119, 191, 1)',
+               'rgba(116, 119, 191, 1)',
+               'rgba(116, 119, 191, 1)',
+               'rgba(116, 119, 191, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+
+    options: {
+
+
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+}
+
+dailyTraffic();
