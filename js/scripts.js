e7abb1f2b813cbcc273cb2a749a35ea1e41e06cc
rdelva <rdelva@users.noreply.github.com>
2019-06-11 18:38:48 -0400

    Added in autocomplete

--- js/scripts.js
+++ js/scripts.js
@@ -32,8 +32,125 @@ $(bell).click(function (){
 });
 
 
+function monthlyTraffic(){
+
+    var ctx = document.getElementById('monthlyChart');
+
+
+var myChart = new Chart(ctx, {
+    type: 'pie',
+      data: {
+           datasets: [{
+                    
+            backgroundColor : [
+              'rgba(129, 201, 143, 1)',
+              'rgba(116, 177, 191, 1)',
+               'rgba(116, 119, 191, 1)',
+            ],             
+            data: [15, 10, 50, 25],
+            borderColor: [
+               'rgba(116, 119, 191, 1)',
+               'rgba(116, 119, 191, 1)',
+               'rgba(116, 119, 191, 1)',
+               'rgba(116, 119, 191, 1)',
+               'rgba(116, 119, 191, 1)',
+               'rgba(116, 119, 191, 1)',
+               'rgba(116, 119, 191, 1)',
+               'rgba(116, 119, 191, 1)',
+               'rgba(116, 119, 191, 1)',
+               'rgba(116, 119, 191, 1)',
+               'rgba(116, 119, 191, 1)',
+               'rgba(116, 119, 191, 1)',
+            ],
+            borderWidth: 1
+        }],
+        labels: ['Cars', 'Motorcycles', 'Bycicles', 'Buses'],  
+    },
+
+    options: {
+
+      responsive:true, 
+      legend: {
+          position: 'bottom',
+        },
+
+        scales: {
+            xAxes: [{
+                gridLines: {
+                  display: false,
+                  drawBorder: false
+                },
+                 ticks: {
+                  display: false
+
+                }
+            }],
+            yAxes: [{
+               gridLines: {
+                  display: false,
+                  drawBorder: false
+                },
+                ticks: {
+                  display: false
+
+                }
+            }]
+        }
+    }
+});
+
+}
+
+
+function dailyTraffic2() {
+
+// javascript
+var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+
+var svgWidth = 500, svgHeight = 300, barPadding = 5;
+var barWidth = svgWidth / dataset.length;
+var svg = d3.select('svg')
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+    
+var barChart = svg.selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("y", function(d) {
+         return svgHeight - d 
+    })
+    .attr("height", function(d) { 
+        return d; 
+    })
+    .attr("width", barWidth - barPadding)
+    .attr("class", "bar")
+    .attr("transform", function (d, i) {
+        var translate = [barWidth * i, 0]; 
+        return "translate("+ translate +")";
+    });
+
+var text = svg.selectAll("text")
+    .data(dataset)
+    .enter()
+    .append("text")
+    .text(function(d) {
+        return d;
+    })
+    .attr("y", function(d, i) {
+        return svgHeight - d - 2;
+    })
+    .attr("x", function(d, i) {
+        return barWidth * i;
+    })
+    .attr("fill", "#A64C38");
+
+
+}
+
+
 function traffic(){
-	var ctx = document.getElementById('trafficChart');
+	var ctx = document.getElementById('hourlyChart');
 var myChart = new Chart(ctx, {
     type: 'line',
     data: {
@@ -195,6 +312,83 @@ var myChart = new Chart(ctx, {
 });
 }
 
+
+
+
+function selectChart(){
+    const time = document.querySelector('.time ul');
+        const hourlyChart = document.getElementById('hourlyChart');
+        const monthlyChart = document.getElementById('monthlyChart');
+        const barchart = document.getElementById('bar-chart');
+        const weeklyChart = document.getElementById('weeklyChart');
+
+
+    const canvas = document.createElement('canvas');
+    
+     hourlyChart.style.display = 'block';
+               dailyChart.style.display = 'none';
+               weeklyChart.style.display = 'none';
+               monthlyChart.style.display = 'none';
+
+    time.addEventListener('click', (e) => {
+          const cdisplay = document.getElementById('chart_display');
+
+
+      if(e.target.tagName == 'LI') {
+
+          if(e.target.id == 'Hourly' ){
+               hourlyChart.style.display = 'block';
+               dailyChart.style.display = 'none';
+               weeklyChart.style.display = 'none';
+               monthlyChart.style.display = 'none';
+
+          } else if(e.target.id == 'Daily'){
+              hourlyChart.style.display = 'none';
+              dailyChart.style.display = 'block';
+              weeklyChart.style.display = 'none';
+              monthlyChart.style.display = 'none';
+
+          } else if(e.target.id == 'Weekly'){
+
+               hourlyChart.style.display = 'none';
+               dailyChart.style.display = 'none';
+               weeklyChart.style.display = 'block';
+               monthlyChart.style.display = 'none';
+
+
+
+          } else if(e.target.id == 'Monthly') {
+               hourlyChart.style.display = 'none';
+               dailyChart.style.display = 'none';
+               weeklyChart.style.display = 'none';
+               monthlyChart.style.display = 'block';
+          }
+      }
+
+    }); //end of click event listener
+
+
+}
+
+$( function() {
+    var availableTags = [
+      "Cindy Wood",
+      "Dan Oliver",
+      "Dawn Wood",
+      "Victora Chambers",
+      "Settings"
+    ];
+    $( "#search" ).autocomplete({
+      source: availableTags
+    });
+  } );
+
+
+
+
 traffic();
 dailyTraffic();
+dailyTraffic2();
+monthlyTraffic();
 mobileUsers();
+selectChart();
