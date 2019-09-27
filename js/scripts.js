f6f59a7fa6a4b031b620c417ba8e4abdc8f662cd
rdelva <rdelva@users.noreply.github.com>
2019-09-26 21:05:06 -0400

    update id names

--- js/scripts.js
+++ js/scripts.js
@@ -49,6 +49,58 @@ function timeNav(){
 
 }// end of timeNav()
 
+function hourlyTraffic(){
+
+var ctx = document.getElementById('hourlyChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        datasets: [{
+            label: '# of Visitors',
+            data: [12, 19, 3, 5, 2, 3,12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+       responsive:true,
+      maintainAspectRatio: true,
+      aspectRatio: 2,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+}// end of hourlyTraffic
+
+
 function monthlyTraffic(){
 
     var ctx = document.getElementById('monthlyChart');
@@ -169,7 +221,7 @@ var text = svg.selectAll("text")
 
 
 function traffic(){
-	var ctx = document.getElementById('hourlyChart');
+	var ctx = document.getElementById('weeklyChart');
 var myChart = new Chart(ctx, {
     type: 'line',
     data: {
@@ -568,6 +620,7 @@ removeSettings();
 messageUsers();
 timeNav();
 traffic();
+hourlyTraffic();
 dailyTraffic();
 dailyTraffic2();
 monthlyTraffic();
